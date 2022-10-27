0996487ec1a6e08d8bc80addadd5a3ddeb79a5c1
bl4ckgermanshepherd <116742512+bl4ckgermanshepherd@users.noreply.github.com>
2022-10-27 20:08:10 +0200

    Update generador_passwords.py

--- generador_passwords.py
+++ generador_passwords.py
@@ -1,3 +1,5 @@
+#Today I learn how to make random passwords, how to encrypt and how to check if a string match with a hash
+
 import random
 
 import werkzeug
@@ -6,15 +8,15 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 minus = "abcdefghijklmnp`qrstuvwxyz"
 mayus = minus.upper()
-numeros = "0123456789"
-simbolos = "@()[]{}*,;/-_¿?.!¡%<#>&+%="
-base = minus+mayus+numeros+simbolos
-longitud = 20
+numbers = "0123456789"
+symbols = "@()[]{}*,;/-_¿?.!¡%<#>&+%="
+base = minus+mayus+numbers+symbols
+large = 20
 
 
-muestra = random.sample(base,longitud)
+muestra = random.sample(base,large)
 password ="".join(muestra)
-password_encriptado = generate_password_hash(password)
-print("{} --> {}".format(password, password_encriptado))
+password_encrypted = generate_password_hash(password)
+print("{} --> {}".format(password, password_encrypted))
 
-print(check_password_hash(password_encriptado, password))
+print(check_password_hash(password_encrypted, password))
